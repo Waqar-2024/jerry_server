e48e10430fcb8f14be441c2fe863d462668da27b
Waqar <waqarahmed1995dob@gmail.com>
2025-02-25 20:33:12 +0200

    checking auto deploy or not

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.post("/upload", upload.fields([{ name: "file" }, { name: "image" }]), async 
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port just for checking ${port}`);
 });
 // Start server
 // app.listen(3000, () => {
